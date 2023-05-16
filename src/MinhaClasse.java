4b81bec293c9f9ac1cfe505af1dba3f7d20a46f3
Jose Marcio Ferreira da Silva <jmarciosilva@gmail.com>
2023-05-16 20:12:04 -0300

    criado algumas variaveis e metodo nomeCompleto com 2 parametros

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,5 +1,18 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("Ola Mundo!!!");
+        //variável constante não deve ser mudado durante a execuçã do programa
+        final String BR = "Brasil";
+        String meuNome = "Jose Marcio";
+        int anoFabricacao = 2023;
+        boolean verdadeira = true;
+        String primeiroNome = "Jose Marcio";
+        String segundoNome = "Ferreira da Silva";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
