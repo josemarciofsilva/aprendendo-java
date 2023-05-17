476b7c3abb40f764b8e7b76c4f6483796cb37dd5
Jose Marcio Ferreira da Silva <jmarciosilva@gmail.com>
2023-05-17 09:22:49 -0300

    Condicional Ternário

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,14 +1,8 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 3;
+        int nota = 8;
 
-        if (nota >= 7) {
-            System.out.println("APROVADO");
-        }else if (nota >= 5 && nota < 7) {
-            System.out.println("PROVA DE RECUPERAÇÃO");
-        }else
-        {
-            System.out.println("REPROVADO");
-        }
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
     }
 }
