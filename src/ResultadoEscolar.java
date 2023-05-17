ff7e69f56f4e39323701b48f45c60be889b61090
Jose Marcio Ferreira da Silva <jmarciosilva@gmail.com>
2023-05-17 09:18:03 -0300

    Condicional Encadeada

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,9 +1,11 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 7;
+        int nota = 3;
 
         if (nota >= 7) {
             System.out.println("APROVADO");
+        }else if (nota >= 5 && nota < 7) {
+            System.out.println("PROVA DE RECUPERAÇÃO");
         }else
         {
             System.out.println("REPROVADO");
