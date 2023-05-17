9c86403049a83de598335c0cdb25837267b9acc4
Jose Marcio Ferreira da Silva <jmarciosilva@gmail.com>
2023-05-17 10:29:07 -0300

    Percorrendo Array com FOREACH

--- src/ExemploForArray.java
+++ src/ExemploForArray.java
@@ -1,9 +1,14 @@
 public class ExemploForArray {
     public static void main(String[] args) {
-        String alunos []= {"FELIPE", "JONA", "JULIA", "MARCOS"};
+        String alunos []= {"FELIPE", "JONAS", "JULIA", "MARCOS"};
 
-        for (int i = 0; i < alunos.length; i++){
-            System.out.println("O aluno no índice " + i + " é " + alunos[i]);
+        // for (int i = 0; i < alunos.length; i++){
+        //     System.out.println("O aluno no índice " + i + " é " + alunos[i]);
+        // }
+
+        for (String aluno : alunos) {
+            System.out.println("Nome do Aluno é: " + aluno);
         }
+
     }
 }
