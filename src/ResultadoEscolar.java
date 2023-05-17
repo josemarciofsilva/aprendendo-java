8e50822f719885a4a0cf542fe1929696579ae3f4
Jose Marcio Ferreira da Silva <jmarciosilva@gmail.com>
2023-05-17 09:06:31 -0300

    Condicional Composta If Else

---
+++ src/ResultadoEscolar.java
@@ -0,0 +1,12 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 7;
+
+        if (nota >= 7) {
+            System.out.println("APROVADO");
+        }else
+        {
+            System.out.println("REPROVADO");
+        }
+    }
+}
